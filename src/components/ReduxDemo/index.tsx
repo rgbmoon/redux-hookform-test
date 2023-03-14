6f93da923379df0f4984ba14a19a75b6d789c269
rgbmoon <sergeiuniverse@gmail.com>
2023-03-14 15:07:05 +0300

    add imports order

---
+++ src/components/ReduxDemo/index.tsx
@@ -0,0 +1,30 @@
+import { Button, Typography } from '@mui/material'
+import React, { FC } from 'react'
+
+import { useAppDispatch, useAppSelector } from '../../hooks'
+import { demoSlice } from '../../store/slices/demoSlice'
+
+const ReduxDemo: FC = () => {
+  const count = useAppSelector((state) => state.demo.value)
+
+  const { increment, decrement } = demoSlice.actions
+
+  const dispatch = useAppDispatch()
+  return (
+    <div className="flex flex-col [&>*]:mb-4 mt-[120px]">
+      <Typography variant="h4" className="text-center">
+        {count}
+      </Typography>
+      <div className="flex gap-2">
+        <Button color="success" variant="outlined" onClick={() => dispatch(increment())}>
+           Плюс сус
+        </Button>
+        <Button color="secondary" variant="outlined" onClick={() => dispatch(decrement())}>
+          Минус сус
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export { ReduxDemo }
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,28 +1,17 @@
+import { AppBar, Typography } from '@mui/material'
 import React from 'react'
 
-import Button from '@mui/material/Button'
-import Typography from '@mui/material/Typography'
-import { useAppDispatch, useAppSelector } from '../hooks'
-import { demoSlice } from '../store/slices/demoSlice'
+import { ReduxDemo } from '../components/ReduxDemo'
 
 function App() {
-  const count = useAppSelector((state) => state.demo.value)
-
-  const { increment, decrement } = demoSlice.actions
-
-  const dispatch = useAppDispatch()
-
   return (
     <div className="flex flex-col items-center w-screen h-screen p-4 [&>*]:mb-4">
-      <Typography variant="h1">{count}</Typography>
-      <div className="flex gap-2">
-        <Button color="success" variant="outlined" onClick={() => dispatch(increment())}>
-           Плюс сус
-        </Button>
-        <Button color="secondary" variant="outlined" onClick={() => dispatch(decrement())}>
-          Минус сус
-        </Button>
-      </div>
+      <AppBar className="py-4">
+        <Typography variant="h3" className="text-center">
+          Тест РТК и хук форм
+        </Typography>
+      </AppBar>
+      <ReduxDemo />
     </div>
   )
 }
